e4630936d5864f0e928f0973c6001f9979d260e4
tetsuakibaba <tetsuaki.baba@gmail.com>
2024-07-03 15:06:08 +0900

    update

--- latency_logger/data/makeplot.py
+++ latency_logger/data/makeplot.py
@@ -1,16 +1,22 @@
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+import itertools
 
 
 def plot_data(csv_files):
-    # plt.figure(figsize=(15, 3))
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(15, 5))
+
+    # カラーマップを作成
+    colors = itertools.cycle(plt.rcParams['axes.prop_cycle'].by_key()['color'])
 
     for csv_file in csv_files:
         # CSVファイルの読み込み
         df = pd.read_csv(csv_file, header=None, names=[
                          'カウント', 'ms', '特記事項'], encoding='utf-8')
+
+        # 次の色を取得
+        color = next(colors)
 
         # プロットの設定
         markersize = 5  # 点の大きさを設定
@@ -18,21 +24,21 @@ def plot_data(csv_files):
         # カウントとmsのデータをプロット
         plot_name = csv_file.replace('.csv', '')
         plt.plot(df['カウント'], df['ms'], marker='o', linestyle='-',
-                 markersize=markersize,  label=f'{plot_name}')
+                 markersize=markersize, label=f'{plot_name}', color=color)
 
         # 特記事項があるプロットにラベルを表示（90度半時計回りで回転）
         # for i, row in df.iterrows():
         #     if pd.notna(row['特記事項']):
         #         plt.annotate(row['特記事項'], (row['カウント'], row['ms']),
-        #                      textcoords="offset points", xytext=(0, 10), ha='center', fontsize=9, color='red', rotation=90)
+        #                      textcoords="offset points", xytext=(0, 10), ha='center', fontsize=12, color=color, rotation=90)
 
     # グラフの設定
-    plt.xlabel('count')
-    plt.ylabel('ms')
-    plt.title('Latency log: 1920x1080@60fps')
-    plt.legend()
+    plt.xlabel('count', fontsize=12)
+    plt.ylabel('ms', fontsize=12)
+    plt.title('Latency log', fontsize=12)
+    plt.legend(fontsize=12)
     plt.grid(True)
-    plt.ylim(bottom=0, top=500)
+    plt.ylim(bottom=0)
 
     # グラフの表示
     plt.show()
